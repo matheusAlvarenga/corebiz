e47c753a2f2553d5f0589b740d599dd2b934c55a
Matheus Alvarenga <matheus@hubway.com.br>
2021-07-20 19:03:02 -0300

    ✨ Marked up header component

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
+import {
+    Container,
+    Logo,
+    Menu,
+    MenuCounter,
+    MenuIcon,
+    MenuItem,
+    MenuLabel,
+    SearchBar,
+    SearchButton,
+    SearchIcon,
+    SearchInput,
+} from '../styles/components/header.styled';
+
 const Header = () => (
-    <div>
-        <h1>Header</h1>
-    </div>
+    <Container>
+        <Logo />
+        <SearchBar>
+            <SearchInput />
+            <SearchButton>
+                <SearchIcon />
+            </SearchButton>
+        </SearchBar>
+        <Menu>
+            <MenuItem>
+                <MenuIcon />
+                <MenuLabel>Minha conta</MenuLabel>
+            </MenuItem>
+            <MenuItem>
+                <MenuIcon />
+                <MenuCounter>1</MenuCounter>
+            </MenuItem>
+        </Menu>
+    </Container>
 );
 
 export default Header;
---
+++ src/styles/components/header.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Logo = styled.img``;
+
+export const SearchBar = styled.div``;
+
+export const SearchInput = styled.input``;
+
+export const SearchButton = styled.button``;
+
+export const SearchIcon = styled.img``;
+
+export const Menu = styled.div``;
+
+export const MenuItem = styled.div``;
+
+export const MenuIcon = styled.img``;
+
+export const MenuLabel = styled.p``;
+
+export const MenuCounter = styled.div``;
