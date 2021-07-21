7c664cad1c5ccfae219faac6d16965350b162116
Matheus Alvarenga <matheus@hubway.com.br>
2021-07-20 22:43:09 -0300

    ✨ Marked up footer component

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+    Contact,
+    ContactCard,
+    ContactIcon,
+    ContactLabel,
+    Container,
+    Location,
+    LocationDivider,
+    LocationLine,
+    LocationTitle,
+    GreyLine,
+    Author,
+    AuthorImg,
+    AuthorLabel,
+    Authors,
+} from '../styles/components/footer.styled';
+
+import mailSvg from '../assets/mail.svg';
+import phoneSvg from '../assets/phone.svg';
+import logo from '../assets/logo-corebiz-branco-cinza.svg';
+import vtex from '../assets/logo-vetex.svg';
 
 const Footer = () => (
-    <div>
-        <h1>Footer</h1>
-    </div>
+    <>
+        <Container>
+            <Location>
+                <LocationTitle>Localização</LocationTitle>
+                <LocationDivider />
+                <LocationLine>
+                    Avenida Andrômeda, 2000. Bloco 6 e 8
+                </LocationLine>
+                <LocationLine>Alphavile SP</LocationLine>
+                <LocationLine>brasil@corebiz.ag</LocationLine>
+                <LocationLine>+55 11 3090 1039</LocationLine>
+            </Location>
+            <Contact>
+                <ContactCard>
+                    <ContactIcon src={mailSvg} />
+                    <ContactLabel>ENTRE EM CONTATO</ContactLabel>
+                </ContactCard>
+                <ContactCard>
+                    <ContactIcon src={phoneSvg} />
+                    <ContactLabel>
+                        FALE COM O NOSSO CONSULTOR ONLINE
+                    </ContactLabel>
+                </ContactCard>
+            </Contact>
+            <Authors>
+                <Author>
+                    <AuthorLabel>Created by</AuthorLabel>
+                    <AuthorImg src={logo} />
+                </Author>
+                <Author>
+                    <AuthorLabel>Powered by</AuthorLabel>
+                    <AuthorImg src={vtex} />
+                </Author>
+            </Authors>
+        </Container>
+        <GreyLine />
+    </>
 );
 
 export default Footer;
