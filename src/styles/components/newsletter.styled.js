649654e527251a3f84a4ef439155a64b95fe3389
Matheus Alvarenga <matheus@hubway.com.br>
2021-07-20 23:08:33 -0300

    💄 Finished desktop styles for news component

---
+++ src/styles/components/newsletter.styled.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.6rem;
+
+    padding: 3.2rem calc((100% - 80rem) / 2);
+
+    background-color: ${props => props.theme.greyBackground};
+`;
+
+export const Title = styled.p`
+    font-family: 'Lato', sans-serif;
+    font-size: 2.2rem;
+    font-weight: 700;
+
+    color: ${props => props.theme.greyDark};
+`;
+
+export const Form = styled.form`
+    width: 100%;
+`;
+
+export const FormGroup = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    height: 4.8rem;
+
+    padding: 0 1.6rem;
+
+    background: ${props => props.theme.white};
+    border-radius: 5px;
+    border: 0;
+
+    color: ${props => props.theme.grey};
+
+    font-family: 'Lato', sans-serif;
+`;
+
+export const Button = styled.button`
+    width: 100%;
+    max-width: 14rem;
+    height: 4.8rem;
+
+    background: ${props => props.theme.black};
+    color: ${props => props.theme.white};
+    border-radius: 5px;
+    border: 0;
+
+    font-family: 'Lato', sans-serif;
+`;
--- src/styles/themes/main.theme.js
+++ src/styles/themes/main.theme.js
@@ -6,6 +6,7 @@ const mainTheme = {
     grey: '#7A7A7A',
     greyDark: '#333333',
     greyBottom: '#C0C0C0',
+    greyBackground: '#F2F2F2',
 };
 
 export default mainTheme;
